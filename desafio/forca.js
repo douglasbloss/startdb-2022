04c8312b536e15eb97be34dd17b099f1afa039f6
Douglas <douglasbloss@gmail.com>
2022-07-21 12:17:39 -0300

    Update forca.js
    aviso de letra repetida, contador de vida funcional, progressão do corpo na forca, atualização nos termos de algumas funções

--- desafio/forca.js
+++ desafio/forca.js
@@ -2,23 +2,21 @@ const inserir = document.getElementById("inserir");
 const testar = document.getElementById("testar");
 const responder = document.getElementById("responder");
 
-let wordExpect = "";
+let letter = "";
 let word = "";
 let response = "";
 let listLetters = [];
 let wordProgress = [];
-let tentativasFalhas = []; // preciso fazer esse array segurar as letras erradas e mostrar elas atualizadas a cada tentativa
 let gameOver = "Game Over";
 let countChance = 0;
 let bodyParts = 0;
 let resultGame = false;
+let oldLetters = [];
 
 // Separa a palavra em um array contendo cada letra
 function splitWord() {
   word = document.getElementById("palavra").value;
-  console.log(word);
   listLetters = word.split("");
-  console.log(listLetters);
   hideWord();
 }
 
@@ -30,28 +28,55 @@ function hideWord() {
   console.log(wordProgress);
 }
 
+// Verifica se a letra já foi utilizada - Se sim, informa que já foi e tenta outra, Se não, adiciona na lista de letras utilizadas e valida se existe na palavra
+function validateLetter() {
+  letter = document.getElementById("letra").value;
+  let valid = false;
+
+  for (let count = 0; count < 26; count++) {
+    if (letter == oldLetters[count]) {
+      valid = true;
+    } else {
+      // Continua
+    }
+  }
+
+  if (valid == true) {
+    console.log("Letra já utlizada, tente outra! " + oldLetters);
+  } else {
+    oldLetters.push(letter);
+    validateNewLetter(letter);
+  }
+}
+
 // Valida se a letra existe em alguma posição do array
-function validateNewLetter() {
-  wordExpect = document.getElementById("letra").value;
+function validateNewLetter(letter) {
   let result = false;
 
   for (let count = 0; count < listLetters.length; count++) {
-    if (wordExpect == listLetters[count]) {
+    if (letter == listLetters[count]) {
       wordProgress[count] = listLetters[count];
       result = true;
     } else {
     }
   }
   if (result == false) {
-    bodyParts++; // por algum motivo as partes estão sendo contadas mesmo quando se acerta a letra na tentativa
+    bodyParts++;
     validateBody();
     console.log("Essa letra não compõe a palavra!");
   }
 
-  console.log(wordProgress);
-  countChance++; // preciso mostrar o valor da contagens de chances a cada alteração para o jogador saber quantas tentativas ainda restam
+  // Verifica se as letras escolhidas completam a palavra. (Valido apenas para palavras com até 6 caracteres)
+  if (word == wordProgress.toString()) {
+    console.log("Acertou! As letras escolhidas completaram a palavra " + word);
+  } else {
+    // Continua
+  }
 
-  validateChances();
+  // Indica o contador total de chances e atualiza e subtrai a cada tentativa falha
+  console.log(wordProgress);
+  countChance--;
+  console.log("Total de tentativas 6 | Restantes: " + countChance);
 }
 
 // Valida a palavra se tentar responder
@@ -76,14 +101,34 @@ function validateChances() {
 
 // Verifica o corpo na forca
 function validateBody() {
+  switch (bodyParts) {
+    case 1:
+      console.log("Na forca: cabeça");
+      break;
+    case 2:
+      console.log("Na forca: cabeça e tronco");
+      break;
+    case 3:
+      console.log("Na forca: cabeça, tronco e braço direito");
+      break;
+    case 4:
+      console.log("Na forca: cabeça, tronco e dois braços");
+      break;
+    case 5:
+      console.log("Na forca: cabeça, tronco, dois braços e perna direita");
+      break;
+    case 6:
+      console.log("Um corpo completo na forca!");
+      break;
+  }
   if (bodyParts == 6) {
     gameOver = true;
     console.log("Game Over");
   } else {
-    // Continue Game
+    validateChances();
   }
 }
 
 inserir.addEventListener("click", splitWord);
-testar.addEventListener("click", validateNewLetter);
+testar.addEventListener("click", validateLetter);
 responder.addEventListener("click", validadeWord);
